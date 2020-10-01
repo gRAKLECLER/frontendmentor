4cdd0e9d5b6bc45b6cf9a0637352b693bb0c4b86
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-10-01 12:14:45 +0200

    finish this challenge

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -1,11 +1,14 @@
 const $share = document.getElementById('share')
 const $pop = document.getElementById('pop')
 
+let click = 0
 
-$share.addEventListener('mouseover', () => {
-  $pop.classList.add('appear')
+$share.addEventListener('click', () => {
+  if(click % 2 === 0){
+    $pop.classList.add('appear')
+  }else{
+    $pop.classList.remove('appear')
+  }
+  click++
 })
 
-$share.addEventListener('mouseleave', () => {
-  $pop.classList.remove('appear')
-})
