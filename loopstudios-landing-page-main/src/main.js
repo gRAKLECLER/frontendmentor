5f59b5cf23aaf3641942f1992fe04264180e70ec
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-01-28 09:46:08 +0100

    delete loopstudio landing page

--- loopstudios-landing-page-main/src/main.js
+++
@@ -1,6 +0,0 @@
-const $burger = document.querySelector('.hero__burgerContainer')
-const $nav = document.querySelector('.hero__headerNav')
-
-$burger.addEventListener('click', () => {
-  $nav.classList.toggle('appear')
-})
