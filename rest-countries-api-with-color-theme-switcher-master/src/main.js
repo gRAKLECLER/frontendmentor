64f630551323111574bbeb3005d156a0b1272614
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-08-05 16:33:30 +0200

    add search flags and starting darkmode

--- rest-countries-api-with-color-theme-switcher-master/src/main.js
+++ rest-countries-api-with-color-theme-switcher-master/src/main.js
@@ -1,6 +1,10 @@
 const $cards = document.querySelector('.cards')
-const $cardId = document.getElementById('cards__cardId')
-const $card = document.querySelectorAll('.cards__card');
+const $header = document.querySelector('.header')
+const $selection = document.querySelector('.selectSection')
+const $darkMode = document.querySelector('.header__darkMode')
+const $input = document.getElementById('input')
+const $card = document.querySelector('.cards__card');
+
 
 const getCountries = () => {
   return fetch('https://restcountries.eu/rest/v2/all')
@@ -27,28 +31,68 @@ const getCountries = () => {
 getCountries()
 
 
-const getCountriesName = () => {
-  return fetch(`https://restcountries.eu/rest/v2/name/Albania`)
-  .then(res => res.json())
-  .then(data => {
+// const getCountriesName = (name) => {
+//   return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+//   .then(res => res.json())
+//   .then(data => {
 
-    data.forEach(element => {
-      $cardId.innerHTML += `
-        <div class="cards__card">
-          <img class="cards__flag" src="${element.flag}" alt="drapeau">
-          <div class="cards__infoCountry">
-            <h2 class="cards__name">${element.name}</h2>
-            <p"><strong>Population : </strong>${element.population}</p>
-            <p><strong>Region : </strong>${element.region}</p>
-            <p><strong>Capital : </strong>${element.capital}</p>
-          </div>
-        </div>
-      `
-    });
-  })
-  .catch(err => {
-    console.log(err.Response.data.message)
-  })
-}
+//     data.forEach(element => {
+//       $cardId.innerHTML += `
+//         <div class="cards__card">
+//           <img class="cards__flag" src="${element.flag}" alt="drapeau">
+//           <div class="cards__infoCountry">
+//             <h2 class="cards__name">${element.name}</h2>
+//             <p"><strong>Population : </strong>${element.population}</p>
+//             <p><strong>Region : </strong>${element.region}</p>
+//             <p><strong>Capital : </strong>${element.capital}</p>
+//           </div>
+//         </div>
+//       `
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err.Response.data.message)
+//   })
+// }
 
 // getCountriesName()
+
+
+$input.addEventListener('input', (e) => {
+  console.log(e.target.value)
+
+  if(e.target.value){
+    return fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
+    .then(res => res.json())
+    .then(data => {
+  
+      console.log(data)
+  
+      data.forEach(element => {
+        $cards.innerHTML = `
+          <div class="cards__card">
+            <img class="cards__flag" src="${element.flag}" alt="drapeau">
+            <div class="cards__infoCountry">
+              <h2 class="cards__name">${element.name}</h2>
+              <p"><strong>Population : </strong>${element.population}</p>
+              <p><strong>Region : </strong>${element.region}</p>
+              <p><strong>Capital : </strong>${element.capital}</p>
+            </div>
+          </div>
+        `
+      });
+    })
+  }else{
+    return getCountries()
+  }
+})
+
+
+// darkMode 
+
+$darkMode.addEventListener('click', () => {
+  $cards.classList.toggle('dark')
+  $selection.classList.toggle('dark')
+  $header.classList.toggle('dark')
+})
+
